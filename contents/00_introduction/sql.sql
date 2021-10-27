c81d840f055a8631dd521efed7edf73ed6dc2e97
jeonggi <hongjeonggi35@gmail.com>
2021-10-27 11:09:53 +0900

    UPDATE SQL DESCRIPTION LOCATION

---
+++ contents/00_introduction/sql.sql
@@ -0,0 +1,19 @@
+ 1. SQL 명령
+ - Structured Query language의 약자
+ - 구조적 언어이기 때문에 비교, 반복문이 존재하지 않음
+ - 변수 사용이 허용되지 않음
+ 
+1) 종류
+ (1) DML(Data Manupulaion Language)
+    - 테이블의 자료를 조작하는 명령문
+    - INSERT, UPDATE, DELETE
+ (2) DCL(Data Control Language)
+    - grant, commit, rollback, revoke
+    - 권한, 저장, 명령취소 등의 기능 수행
+ (3) DDL(Data Defintion Language)
+    - 데이터 정의어
+    - CREATE, ALTER, DROP
+ (4) 질의어
+    - 데이터 조회기능 수행
+    - SELECT 
+ 
